35a6c5e31c65e05babeeeb561570f04ed3d082f3
Egharevba Gift <egharevbagift79@gmail.com>
2024-09-10 20:03:19 +0100

    [payment]

--- payment-screen/next.config.mjs
+++ payment-screen/next.config.mjs
@@ -1,4 +1,19 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      // Modify the Webpack config to handle CSS files
+      config.module.rules.push({
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
